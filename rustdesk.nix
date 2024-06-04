b6fdb798a1e9c90d23e840896d816b9260c6beb4
SamLukeYes <samlukeyes123@gmail.com>
2024-06-04 21:36:17 +0800

    hbbs: restart on failure

--- rustdesk.nix
+++ rustdesk.nix
@@ -13,14 +13,17 @@
   };
 
   systemd.services.rustdesk-signal = {
-    serviceConfig.ExecStart = lib.mkForce "${pkgs.writeShellScript "hbbs-start" ''
-      PATH=${lib.makeBinPath [
-        config.services.rustdesk-server.package
-        pkgs.curl
-        pkgs.jq
-      ]}:$PATH
-      public_ip=`curl -s -H Metadata:true --noproxy * http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0?api-version=2024-03-15 | jq -r .publicIpAddress`
-      exec hbbs -r $public_ip
-    ''}";
+    serviceConfig = {
+      ExecStart = lib.mkForce "${pkgs.writeShellScript "hbbs-start" ''
+        PATH=${lib.makeBinPath [
+          config.services.rustdesk-server.package
+          pkgs.curl
+          pkgs.jq
+        ]}:$PATH
+        public_ip=`curl -s -H Metadata:true --noproxy * http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0?api-version=2024-03-15 | jq -r .publicIpAddress`
+        exec hbbs -r $public_ip
+      ''}";
+      Restart = "on-failure";
+    };
   };
 }
