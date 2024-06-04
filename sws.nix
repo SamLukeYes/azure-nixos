da8cf7d4f41385a005f94051b84e4385948d6c55
SamLukeYes <samlukeyes123@gmail.com>
2024-06-04 21:52:15 +0800

    Open port for sws

--- sws.nix
+++ sws.nix
@@ -1,6 +1,10 @@
-let rootDir = "/home/azureuser/Public/"; in
+let
+  rootDir = "/home/azureuser/Public/";
+in
 
 {
+  networking.firewall.allowedTCPPorts = [ 8787 ];
+
   programs.rust-motd.settings.service_status.sws = "static-web-server";
 
   services.static-web-server = {
