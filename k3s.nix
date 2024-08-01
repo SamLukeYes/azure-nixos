84dfeece8c240eae855e89e2dc09ef15d152f83c
SamLukeYes <samlukeyes123@gmail.com>
2024-08-02 00:37:30 +0800

    Enable k3s

---
+++ k3s.nix
@@ -0,0 +1,4 @@
+{
+  programs.rust-motd.settings.service_status.k3s = "k3s";
+  services.k3s.enable = true;
+}
--- system.nix
+++ system.nix
@@ -5,6 +5,7 @@
     "${modulesPath}/virtualisation/azure-common.nix"
     "${modulesPath}/virtualisation/azure-image.nix"
     ./fail2ban.nix
+    ./k3s.nix
     ./motd.nix
     ./rustdesk.nix
     ./shadowsocks.nix
