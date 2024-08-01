7f3a7c76be7bb64406e56ebff52cb10edacaebc7
SamLukeYes <samlukeyes123@gmail.com>
2024-08-02 01:37:41 +0800

    Add nerdctl

--- k3s.nix
+++ k3s.nix
@@ -1,4 +1,14 @@
+{ pkgs, ... }:
+
 {
+  environment = {
+    systemPackages = with pkgs; [
+      nerdctl
+    ];
+
+    sessionVariables.CONTAINERD_ADDRESS = "unix:///run/k3s/containerd/containerd.sock";
+  };
+
   programs.rust-motd.settings.service_status.k3s = "k3s";
   services.k3s.enable = true;
 }
