80c52c228f3c25cdb5c864932c6ab4d81c33212a
SamLukeYes <samlukeyes123@gmail.com>
2024-08-02 00:45:21 +0800

    Update fastfetch args

--- motd.nix
+++ motd.nix
@@ -6,7 +6,7 @@
     settings = {
       banner = {
         color = "white";
-        command = "${lib.getExe pkgs.fastfetch} --colors-block-range-end 0 --logo-type none --ts-version false";
+        command = "${lib.getExe pkgs.fastfetch} --colors-block-range-end 0 --logo-type none --detect-version false";
       };
       # filesystems.root = "/";
       last_login.azureuser = 10;
