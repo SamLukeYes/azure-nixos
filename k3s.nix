79c625f69f23e58868cfc41e3a4bf4b432bd1cc0
SamLukeYes <samlukeyes123@gmail.com>
2024-08-03 01:32:20 +0800

    Remove nerdctl

--- k3s.nix
+++ k3s.nix
@@ -1,14 +1,6 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-  environment = {
-    systemPackages = with pkgs; [
-      nerdctl
-    ];
-
-    sessionVariables.CONTAINERD_ADDRESS = "unix:///run/k3s/containerd/containerd.sock";
-  };
-
   programs.rust-motd.settings.service_status.k3s = "k3s";
   services.k3s.enable = true;
 }
