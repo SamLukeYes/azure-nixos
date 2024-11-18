4c3377567601a3ec9f7ed49313f70cebb4c63335
SamLukeYes <samlukeyes123@gmail.com>
2024-11-19 01:36:21 +0800

    Show waagent status on motd

--- motd.nix
+++ motd.nix
@@ -8,8 +8,8 @@
         color = "white";
         command = "${lib.getExe pkgs.fastfetch} --logo-type none --detect-version false";
       };
-      # filesystems.root = "/";
       last_login.azureuser = 10;
+      service_status.waagent = "waagent";
     };
   };
 }
